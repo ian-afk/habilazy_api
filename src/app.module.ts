9db4fd1501f54d3dea7cb423ed882690afe6e175
ian-afk <crkdosal@gmail.com>
2026-01-06 20:16:51 +0800

    chore(task): task initial state

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     UsersModule,
     PrismaModule,
+    TaskModule,
   ],
 })
 export class AppModule {}
