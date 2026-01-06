c78d46d86271f2126c625887db10b925e32137fb
ian-afk <crkdosal@gmail.com>
2026-01-06 20:28:02 +0800

    chore (task): import prismamodule

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { TaskRepository } from './repositories/task.repository';
 
 @Module({
-  providers: [TaskService],
+  imports: [PrismaModule],
+  providers: [TaskService, TaskRepository],
   controllers: [TaskController],
 })
 export class TaskModule {}
