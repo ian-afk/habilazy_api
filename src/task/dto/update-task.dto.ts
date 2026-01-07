d8ef502ea9989c4539f7c299f2f9362610ce0b86
ian-afk <crkdosal@gmail.com>
2026-01-08 07:09:30 +0800

    chore (task): add cl validator for updatetaskDto and change userId to mandatory

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class UpdateTaskDto {
+  @IsString()
   task?: string;
   description?: string;
-  userId?: number;
+
+  @IsNotEmpty()
+  userId: number;
 }
