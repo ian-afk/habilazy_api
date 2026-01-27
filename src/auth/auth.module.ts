7aa82075bac27ab042327f1eb71d41a192a7e4d1
ian-afk <crkdosal@gmail.com>
2026-01-28 06:13:24 +0800

    chore (auth): import user module and add jwtmodule for jwtstrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { UsersService } from 'src/users/users.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PassportModule.register({ session: false })],
-  providers: [UsersService, JwtStrategy, AuthService, UsersService],
+  imports: [
+    PassportModule.register({ session: false }),
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET, // or hardcode for now
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [JwtStrategy, AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
