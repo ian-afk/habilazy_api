f98c5eecfef646f62c0ceb20434115d276039665
ian-afk <crkdosal@gmail.com>
2026-01-28 06:13:32 +0800

    chore (auth): import user module and add jwtmodule for jwtstrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { UsersService } from 'src/users/users.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
